7b49c6d5ce179e4304bed99c1f891fe45846a53f
morgan <morgan.malavsi@gmail.com>
2019-10-05 02:24:22 +0200

    start admin

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/user', require('./userApi.js'));
 router.use('/courses/book', require('./coursesApi.js'));
+router.use('/admin/log', require('./adminApi.js'));
 
 module.exports = router;
---
+++ routes/utils/utilsAdmin.js
@@ -0,0 +1,7 @@
+const MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+const uri = "mongodb+srv://morgan:admin@musickinesis-x2kkv.mongodb.net/test?retryWrites=true&w=majority";
+
+
+
+module.exports = {};
